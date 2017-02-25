463347931ff9926981edd4db6e66cc2ea51eeb6d
justcode247 <justcode247@gmail.com>
2017-02-25 22:42:29 +0000

    Fixed the questions page and implemented the looping functionality fot multiple questions randomly.

--- js/questions.js
+++ js/questions.js
@@ -1,34 +1,52 @@
 var current_response = undefined;
+var random = undefined;
 var rightstring = 'Yes you are right!';
 var wrongstring = "Sorry, that is actually not true";
-// stereotypes stored in stereotypes.js
-var stereotypes = [ screen];
-var random = undefined;
-  
+var nextButton = '<button type="button" id="next" class="btn btn-primary btn-lg btn-block" onclick="ask()">Next</button>'
+
+
+// function init(){
+//     random = Math.floor((Math.random() * st.length));
+//     document.getElementById("question").innerHTML = st[random]['q'];
+// }
+
 
 function ask(){
-    random = Math.floor((Math.random() * stereotypes.length));
-    document.getElementById("question").innerHTML = stereotypes[random]['q'];
+    console.log('asked')
+    var rr = document.getElementById("right-wrong");
+    var next = document.getElementById("next")
+    var details  = document.getElementById("details") 
+    rr.innerHTML = ''
+    next.outerHTML = '<div id="next"></div>'
+    details.innerHTML = ''
+    random = Math.floor((Math.random() * st.length));
+    document.getElementById("question").innerHTML = st[random]['q'];
 }
 
 // on clicking true
 function ontrue(){
     current_response = 1
     checkAnswer()
+    showNext()
 }
 // onclicking false
 function onfalse(){
     current_response = 0
     checkAnswer()
+    showNext()
 }
 // checks answers after clicking
 function checkAnswer(){
-    if (current_response == stereotypes[random]['correct']) {
+    if (current_response == st[random]['correct']) {
         document.getElementById("right-wrong").innerHTML = rightstring;
     } else { 
         document.getElementById("right-wrong").innerHTML = wrongstring;
     }
-    document.getElementById("details").innerHTML = stereotypes[random]['a'];
+    document.getElementById("details").innerHTML = st[random]['a'];
 }
 
+function showNext(){
+    var next = document.getElementById("next");
+    next.outerHTML = nextButton;
+}
 ask();
