75dff6f5ed1b2f99a6aaf3af1717827e95f49570
justcode247 <justcode247@gmail.com>
2017-02-25 20:25:36 +0000

    Changed the questions implementation in questions.js. linked new questions.html to the website stereotypes.js will contain all stereotypes which are stored in the array in questions.js. More questions yet to be added and getting next question functionality yet to be added.

--- js/questions.js
+++ js/questions.js
@@ -1,57 +1,34 @@
-var questionnaire = [
-  {
-    "question" : "The Earth is round.",
-    "response" : "The Earth is round!",  
-    "correct"  : 1    // 0=False, 1=True
-  },
-  {
-    "question" : "The 'cravat' is originally from France.",
-    "response" : "The 'cravat' is from Croatia!",
-    "correct"  : 0
-  },
-  {
-    "question" : "Is Java == JavaScript?",
-    "response" : "It's a different language.",
-    "correct"  : 0
-  } // Add comma and more objects.
-];
+var current_response = undefined;
+var rightstring = 'Yes you are right!';
+var wrongstring = "Sorry, that is actually not true";
+// stereotypes stored in stereotypes.js
+var stereotypes = [ screen];
+var random = undefined;
+  
 
-var $qDIV     = $('#qDIV'),
-    $rDIV     = $('#response'),
-    $qH2      = $("h2",     $qDIV),
-    $answer   = $("button", $qDIV),
-    $response = $("p",      $rDIV),
-    tot       = questionnaire.length,
-    c         = 0; // Current Q array counter 
-
-function QandA( idx ){  
-   $qDIV.fadeTo(600,1); 
-   $rDIV.hide();  
-   var currQ = questionnaire[c];   // The Object literal from Array
-   var isCorrect = currQ.correct;  // 0 or 1?
-   var answerIsCorrect = idx==isCorrect; // (compare values) Returns boolean 
-   var resp = answerIsCorrect ? "Great!" : "Wrong!";
-   currQ.answer = idx;             // Put user answer into object (0 or 1)
-   $qH2.text( (c+1) +'. '+ currQ.question );
-   $response.text( resp +' '+ currQ.response );
+function ask(){
+    random = Math.floor((Math.random() * stereotypes.length));
+    document.getElementById("question").innerHTML = stereotypes[random]['q'];
 }
-QandA();
 
-$answer.click(function(){   
-    var idx = $answer.index(this); // 0 or 1  (get button index)
-    QandA( idx );
-    $rDIV.fadeTo(600,1);
-    $qDIV.hide();
-    console.log( JSON.stringify(questionnaire, null, 1) ); // TEST ONLY
-});
+// on clicking true
+function ontrue(){
+    current_response = 1
+    checkAnswer()
+}
+// onclicking false
+function onfalse(){
+    current_response = 0
+    checkAnswer()
+}
+// checks answers after clicking
+function checkAnswer(){
+    if (current_response == stereotypes[random]['correct']) {
+        document.getElementById("right-wrong").innerHTML = rightstring;
+    } else { 
+        document.getElementById("right-wrong").innerHTML = wrongstring;
+    }
+    document.getElementById("details").innerHTML = stereotypes[random]['a'];
+}
 
-$('#prev, #next').click(function(){
-    c = this.id=='next' ? ++c : c ; // advance or repeat Question
-    QandA();
-    $('#next').toggle(c<tot-1);
-    $('#score').toggle(c>=tot-1);
-});
-
-$('#score').click(function(){
-  $('pre').text( JSON.stringify(questionnaire, null, 2) ); 
-});
+ask();
---
+++ js/stereotypes.js
@@ -0,0 +1,8 @@
+var screen = {
+        "q": "You are looking at this screen",
+        "a": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula turpis ipsum, eget pellentesque mauris commodo in. Sed dui justo, pellentesque vel nibh ac, mollis tristique urna. Sed blandit leo ultrices velit vestibulum, eget ultricies sapien gravida. In sed justo ut nunc porta ornare in sed tortor. Curabitur mi sapien, pretium pellentesque metus a, volutpat auctor eros. Proin tincidunt dictum sapien, eget mollis risus vestibulum suscipit. Integer lacus mi, commodo at quam vitae, fringilla hendrerit est. Duis imperdiet tellus vel nunc consequat, quis malesuada est commodo. Aliquam nec bibendum leo, vulputate euismod arcu. Ut ut massa sit amet nibh egestas commodo et id ligula. Ut at fermentum nibh. Duis at laoreet mauris. Curabitur a malesuada augue, vitae rutrum enim. Duis sit amet quam ultrices, interdum justo non, egestas ante.",
+        "correct": 1
+    },
+    muslim = {
+        
+    }
